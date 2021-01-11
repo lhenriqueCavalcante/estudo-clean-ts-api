bbb58ec105ff95fb89fad78606f034da708bb190
Luiz Henrique <henriquerjsg@hotmail.com>
2021-01-11 15:45:39 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('content Type Middleware', () => {
+  test('should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.type('json')
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+})
