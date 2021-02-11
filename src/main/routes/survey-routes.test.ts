556ddc4e80b1d1e2d816f1dec4584af8e5da83a1
Luiz Henrique <henriquerjsg@hotmail.com>
2021-02-11 18:32:30 -0300

    refactor: move accessToken creation to a facotry method

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -8,6 +8,26 @@ import env from '../config/env'
 let surveyCollection: Collection
 let accountCollection: Collection
 
+const makeToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Henrique',
+    email: 'henrique@mail.com',
+    password: '123',
+    role: 'admin'
+  })
+  const id = res.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+
+  return accessToken
+}
+
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -40,21 +60,7 @@ describe('Survey Routes', () => {
     })
 
     test('should return 204 on add survey with valid accessToken', async () => {
-      const res = await accountCollection.insertOne({
-        name: 'Henrique',
-        email: 'henrique@mail.com',
-        password: '123',
-        role: 'admin'
-      })
-      const id = res.ops[0]._id
-      const accessToken = sign({ id }, env.jwtSecret)
-      await accountCollection.updateOne({
-        _id: id
-      }, {
-        $set: {
-          accessToken
-        }
-      })
+      const accessToken = await makeToken()
       await request(app)
         .post('/api/surveys')
         .set('x-access-token', accessToken)
@@ -79,20 +85,7 @@ describe('Survey Routes', () => {
     })
 
     test('should return 204 on load surveys with valid accessToken if there are no surveys', async () => {
-      const res = await accountCollection.insertOne({
-        name: 'Henrique',
-        email: 'henrique@mail.com',
-        password: '123'
-      })
-      const id = res.ops[0]._id
-      const accessToken = sign({ id }, env.jwtSecret)
-      await accountCollection.updateOne({
-        _id: id
-      }, {
-        $set: {
-          accessToken
-        }
-      })
+      const accessToken = await makeToken()
       await request(app)
         .get('/api/surveys')
         .set('x-access-token', accessToken)
