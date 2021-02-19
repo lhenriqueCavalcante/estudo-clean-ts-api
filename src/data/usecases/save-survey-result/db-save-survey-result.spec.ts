786016c5674877b22cd72ca1136cd48d61853fda
Luiz Henrique <henriquerjsg@hotmail.com>
2021-02-19 18:24:42 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,51 @@
+import { DbSaveSurveyResult } from './db-save-survey-result'
+import { SaveSurveyResultRepository, SurveyResultModel, SaveSurveyResultModel } from './db-save-survey-result-protocols'
+import MockDate from 'mockdate'
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_answer',
+  date: new Date()
+})
+
+const makeFakeSurveyResult = (): SurveyResultModel => Object.assign({}, makeFakeSurveyResultData(), {
+  id: 'any_id'
+})
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return await new Promise(resolve => resolve(makeFakeSurveyResult()))
+    }
+  }
+  return new SaveSurveyResultRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+  return { saveSurveyResultRepositoryStub, sut }
+}
+
+describe('DbSaveSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+  test('Should call SaveSurveyResultRepository with correct values', async () => {
+    const { saveSurveyResultRepositoryStub, sut } = makeSut()
+    const addSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+    const surveyResultData = makeFakeSurveyResultData()
+    await sut.save(surveyResultData)
+    expect(addSpy).toHaveBeenCalledWith(surveyResultData)
+  })
+})
